a61d62d49e311416c5cda681575f9ee09c42dabd
Matthew Thomas <me@matthewathomas.com>
2025-12-23 21:53:38 -0500

    Back to brightening it

--- WpfApp/AggregateShader.cs
+++ WpfApp/AggregateShader.cs
@@ -25,6 +25,6 @@ public readonly partial struct AggregateShader(
         }
         var newMedianPixel = numerator / denominator;
         aggregate[ThreadIds.XY] = newMedianPixel;
-        output[ThreadIds.XY] = Hlsl.Pow(newMedianPixel, 2f);
+        output[ThreadIds.XY] = Hlsl.Pow(newMedianPixel, 0.4f);
     }
 }
